74b1a8bdee8562e93c8c9e1d5d40bed56d36af30
reese <reeseyh18@gmail.com>
2025-09-18 14:16:14 -0400

    player movement fixes

--- SunnySideStudio-MyaamiaGame/Assets/Scripts/Player.cs
+++ SunnySideStudio-MyaamiaGame/Assets/Scripts/Player.cs
@@ -12,18 +12,14 @@ public class Player : MonoBehaviour
     private float speed = 3f;
     private float jumpForce = 20f;
     private float moveHorizontal;
+    private float moveVertical;
     private bool isJumping;
 
     void Update()
     {
         //get move input
         moveHorizontal = Input.GetAxisRaw("Horizontal");
-
-        //jump
-        if (!isJumping && Input.GetButtonDown("Jump"))
-        {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-        }
+        moveVertical = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
@@ -32,6 +28,11 @@ public class Player : MonoBehaviour
         if(moveHorizontal !=0)
         {
             rb.AddForce(new Vector2(moveHorizontal * speed, 0), ForceMode2D.Impulse);
+        }
+        //jump
+        if(!isJumping && moveVertical > 0)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
     }
 
