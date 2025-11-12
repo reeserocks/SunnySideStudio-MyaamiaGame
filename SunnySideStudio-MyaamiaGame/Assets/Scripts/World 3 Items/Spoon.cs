eb0d046fd03ff27be44c60b92d475eb7f0cbc6ca
Arnoldnl23 <arnoldnl@miamioh.edu>
2025-11-12 16:55:32 -0500

    Spoon fix for real

--- SunnySideStudio-MyaamiaGame/Assets/Scripts/World 3 Items/Spoon.cs
+++ SunnySideStudio-MyaamiaGame/Assets/Scripts/World 3 Items/Spoon.cs
@@ -45,42 +45,6 @@ public class Spoon : ObjectParent
         }
     }
 
-    new void Update()
-    {
-        if (!(Input.GetKeyDown(KeyCode.Return)))
-        {
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-            }
-            moveHorizontal = Input.GetAxisRaw("Horizontal");
-            moveVertical = Input.GetAxisRaw("Vertical");
-            rb.AddForce(new Vector2(moveHorizontal * speed, 0), ForceMode2D.Impulse);
-            rb.AddForce(new Vector2(0, moveVertical * speed), ForceMode2D.Impulse);
-            capVelocity();
-        }
-        else
-        {
-            if (!isColliding)
-            {
-                //rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-                rb.gravityScale = 1;
-                thisCollision.isTrigger = false;
-                GameManager.isPlacing = false;
-                GameManager.canType = true;
-                //play successsful spawn sound
-                this.enabled = false;
-                spriteRen.color = new Color(1, 1, 1, 1f);
-            }
-            else
-            {
-                AudioSource.PlayClipAtPoint(Resources.Load<AudioClip>("failure_summon"), Camera.main.transform.position);
-                spriteRen.color = new Color(1, 0, 0, .5f);
-                StartCoroutine(ColorFlash());
-            }
-        }
-    }
-
     void OnTriggerEnter2D(Collider2D c)
     {
         if (c.CompareTag("Player"))
